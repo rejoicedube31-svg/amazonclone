59efee2cce6d9287c09a4152c6e286f075b20008
rejoice dube <rejoicedube31@gmail.com>
2026-05-01 14:38:55 +0200

    Normalize line endings

---
+++ src/app/pages/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <>
+        <NavBar />
+        
+        <Footer/>
+    </>
+    )
+}
+
+export default Home;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,60 @@
+const NavBar = () => {
+  
+    const categoryList = ["All", "Arts & Crafts", "Automotive", "Baby", "Books", "Computers", "Electronics", "Garden & Outdoor", "Grocery", "Health & Personal Care", "Home & Kitchen", "Industrial & Scientific", "Luggage", "Movies & TV", "Music, CDs & Vinyl", "Pet Supplies", "Software", "Sports & Outdoors", "Tools & Home Improvement", "Toys & Games", "Video Games"];
+
+    return (
+    <>
+        <img src="" alt="logo" />
+        
+        <section className="nav-group">
+            <img src="" alt="location" />
+            <p>Deliver to</p>
+            <h5>South Africa</h5>
+        </section>
+
+        <section className="search">
+            <select>
+                {
+                    categoryList.map((category) => (
+                    <option >{category}</option>
+                    ))
+                }
+            </select>
+
+            <input type="text" placeholder="Search Amazon" />
+
+            <button>
+                <img src="" alt="search" />
+            </button>
+
+        </section>
+        
+        <section className="nav-group">
+            <option value="en_US">English - EN</option>
+            <option value="es_US">español - ES</option>
+            <option value="ar_AE">العربية - AR</option>
+            <option value="de_DE">Deutsch - DE</option>
+            <option value="he_IL">עברית - HE</option>
+            <option value="ko_KR">한국어 - KO</option>
+            <option value="pt_BR">português - PT</option>
+            <option value="zh_CN">中文 (简体) - ZH</option>
+            <option value="zh_TW">中文 (繁體) - ZH</option>
+            
+        </section>
+        
+        <section className="nav-group">
+            <p>Hello, Sign in</p>
+            <h5>Account & Lists</h5>
+        </section>
+
+        <section className="nav-group">
+            <p>Returns</p>
+            <h5>& Orders</h5>
+        </section>
+    </>
+
+  )
+
+}
+
+export default NavBar;
